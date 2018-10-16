015eb2864477cd9a1e1425b1356aa77d75533694
mafeq03 <mafeq03@gmail.com>
2018-10-15 21:19:38 -0400

    reviewing material

---
+++ starter/script.js
@@ -0,0 +1,31 @@
+console.log('Hello World!!!');
+
+var firstName = 'Mafeezy';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 29;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+job = 'Teacher';
+console.log(job);
+
+console.log(firstName + ' ' + age);
+
+//Declare many variables in many lines and then define them
+
+var job, isMarried;
+job = 'paralegal';
+isMarried = 'almost'; 
+
+//variable mutation
+age = 'twenty eight';
+job = 'clown';
+
+alert(firstName + ' is a ' + age + ' years old ' + job + '. Is she married?' + isMarried)
+
+var lastName = prompt('What is her last Name');
+console.log(firstName + ' ' + lastName);
