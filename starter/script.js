780aae7469f4eaca30653b4037ec6c341fd64df0
mafeq03 <mafeq03@gmail.com>
2018-10-20 16:56:15 -0400

    Saturday

--- starter/script.js
+++ starter/script.js
@@ -31,10 +31,32 @@ var lastName = prompt('What is her last Name');
 console.log(firstName + ' ' +  lastName);
 
 //Basic Operators
+var year, yearJohn, yearMark, ageJohn, ageMark;
+ageJohn = 28;
+ageMark = 33;
 
-var year = 2018;
-var yearJohn = year - 28;
-var 
+now = 2018;
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+console.log(yearJohn);
 
-//Math Operators
-var year 
+console.log(now + 2);
+console.log(now *2);
+console.log(now / 10);
+
+//Logical Operators
+var johnOlder = ageJohn > ageMark;
+
+console.log(johnOlder);
+
+//typeof operator
+//will describe the type of the variable
+console.log(typeof johnOlder);
+
+console.log(typeof ageJohn);
+
+//Operator Precedence
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
