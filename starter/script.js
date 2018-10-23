b4e757afae2d90528066860491126b3273025a37
mafeq03 <mafeq03@gmail.com>
2018-10-22 22:23:01 -0400

    Monday

--- starter/script.js
+++ starter/script.js
@@ -80,3 +80,29 @@ x +=2;
 console.log(x);
 x+=10;
 console.log(x);
+//will add one unit to the variable same as
+//x+=1 or x=x+1
+x++
+
+/*****************************
+* CODING CHALLENGE 1
+*/
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
+
+GOOD LUCK 😀
+*/
+//Their height and mass
+var markHeight = 1.75;
+var johnHeight = 1.8;
+
+var markMass = markHeight ** 2;
+var johnMass = johnHeight ** 2;
+
+
