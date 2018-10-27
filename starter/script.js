6acba9dcd677bc3e580cf73e682f48d6ae2e5cb5
mafeq03 <mafeq03@gmail.com>
2018-10-26 22:44:02 -0400

    PTII

--- starter/script.js
+++ starter/script.js
@@ -117,3 +117,8 @@ console.log(johnBmi);I = BMIMark > BMIJohn;
 var firstName = 'John';
 var civilStatus = 'single';
 
+if (civilStatus === 'married'){
+  console.log(firstName + 'is married');
+} else{
+  console.log(firstName + 'will hopefully marry soon :)');
+}  
