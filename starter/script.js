6de8dede57bb550ad6b81947aff98284965f1d39
mafeq03 <mafeq03@gmail.com>
2018-10-29 22:27:44 -0400

    Monday

--- starter/script.js
+++ starter/script.js
@@ -135,3 +135,15 @@ if (isMarried){
   }else {
     console.log(firstName + 'will hopefully marry soon :)')
   }
+
+//Boolean logic//
+var firstName = 'Eric';
+var age = 18;
+
+if (age < 13){
+  console.log(firstName + 'is a boy.');
+  } else if(age >= 13 && 20){
+  console.log (firstName + 'is a teenager.');
+  } else {
+  console.log()
+  }
